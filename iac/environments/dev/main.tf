958f58e2e46143e4016551c6d67619f0f0a076e7
Eddu <azabacheeduardo85@gmail.com>
2026-04-26 08:45:07 -0500

    feat: add DynamoDB module configuration to main.tf

--- iac/environments/dev/main.tf
+++ iac/environments/dev/main.tf
@@ -3,3 +3,8 @@ module "cognito" {
   name_prefix       = "${var.project_name}-${var.environment}"
   app_email_subject = "${var.project_name} - Verify your email"
 }
+
+module "dynamodb" {
+  source      = "../../modules/dynamodb"
+  name_prefix = "${var.project_name}-${var.environment}"
+}
