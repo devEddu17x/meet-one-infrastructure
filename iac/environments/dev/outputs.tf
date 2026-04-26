b628a3c8e7b1cecc45651d91dea84c37d1e40547
Eddu <azabacheeduardo85@gmail.com>
2026-04-26 08:44:34 -0500

    feat: add outputs for DynamoDB tables (users, connections, matchmaking)

--- iac/environments/dev/outputs.tf
+++ iac/environments/dev/outputs.tf
@@ -17,3 +17,33 @@ output "frontend_client_id" {
   description = "Client ID del User Pool Client para el frontend"
   value       = module.cognito.frontend_client_id
 }
+
+output "dynamodb_users_table_name" {
+  description = "The name of the users table"
+  value       = module.dynamodb.dynamodb_users_table_name
+}
+
+output "dynamodb_users_table_arn" {
+  description = "The ARN of the users table"
+  value       = module.dynamodb.dynamodb_users_table_arn
+}
+
+output "dynamodb_connections_table_name" {
+  description = "The name of the connections table"
+  value       = module.dynamodb.dynamodb_connections_table_name
+}
+
+output "dynamodb_connections_table_arn" {
+  description = "The ARN of the connections table"
+  value       = module.dynamodb.dynamodb_connections_table_arn
+}
+
+output "dynamodb_matchmaking_table_name" {
+  description = "The name of the matchmaking table"
+  value       = module.dynamodb.dynamodb_matchmaking_table_name
+}
+
+output "dynamodb_matchmaking_table_arn" {
+  description = "The ARN of the matchmaking table"
+  value       = module.dynamodb.dynamodb_matchmaking_table_arn
+}
