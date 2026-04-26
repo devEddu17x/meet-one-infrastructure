fb7b356bb3d18705bf7f565de8429215d75a6e2b
Eddu <azabacheeduardo85@gmail.com>
2026-04-25 23:27:31 -0500

    fix: update Cognito outputs to use module references in dev environment

--- iac/environments/dev/outputs.tf
+++ iac/environments/dev/outputs.tf
@@ -1,20 +1,19 @@
-
 output "user_pool_id" {
   description = "ID del Cognito User Pool"
-  value       = aws_cognito_user_pool.pool.id
+  value       = module.cognito.user_pool_id
 }
 
 output "user_pool_arn" {
   description = "ARN del Cognito User Pool"
-  value       = aws_cognito_user_pool.pool.arn
+  value       = module.cognito.user_pool_arn
 }
 
 output "user_pool_endpoint" {
   description = "Endpoint del Cognito User Pool"
-  value       = aws_cognito_user_pool.pool.endpoint
+  value       = module.cognito.user_pool_endpoint
 }
 
 output "frontend_client_id" {
   description = "Client ID del User Pool Client para el frontend"
-  value       = aws_cognito_user_pool_client.frontend_client.id
+  value       = module.cognito.frontend_client_id
 }
