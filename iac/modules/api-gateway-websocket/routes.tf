76a11fb26cb65056a52865e456baeb18f46dea89
Eddu <azabacheeduardo85@gmail.com>
2026-04-30 12:54:42 -0500

    feat: enable disconnect route in API Gateway WebSocket module

--- iac/modules/api-gateway-websocket/routes.tf
+++ iac/modules/api-gateway-websocket/routes.tf
@@ -9,7 +9,8 @@ resource "aws_apigatewayv2_route" "route_connect" {
   target    = "integrations/${aws_apigatewayv2_integration.integration_connect.id}"
 }
 
-# resource "aws_apigatewayv2_route" "route_disconnect" {
-#   api_id    = aws_apigatewayv2_api.api_websocket.id
-#   route_key = "$disconnect"
-# }
+resource "aws_apigatewayv2_route" "route_disconnect" {
+  api_id    = aws_apigatewayv2_api.api_websocket.id
+  route_key = "$disconnect"
+  target    = "integrations/${aws_apigatewayv2_integration.integration_disconnect.id}"
+}
